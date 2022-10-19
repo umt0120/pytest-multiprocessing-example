da538d35ab85285d080373e5b04ece19cbfcf0ac
umt0120 <uemoto0120@gmail.com>
2022-10-19 21:12:38 +0900

    fix: format with black

--- main.py
+++ main.py
@@ -21,13 +21,17 @@ def test_print_synchronously(capfd: CaptureFixture) -> None:
     print_synchronously()
 
     out, err = capfd.readouterr()
-    
-    assert str(out).split("\n")[0] == "0 番目のメッセージをprintします ... [print_synchronouslyから呼び出されました。]"
+
+    assert (
+        str(out).split("\n")[0]
+        == "0 番目のメッセージをprintします ... [print_synchronouslyから呼び出されました。]"
+    )
+
 
 def test_print_in_multi_process(capfd: CaptureFixture) -> None:
     print_in_multi_process()
 
     out, err = capfd.readouterr()
     sorted_out = sorted(str(out).split("\n"))
-    
+
     assert sorted_out[0] == "0 番目のメッセージをprintします ... [print_in_multi_processから呼び出されました]"
